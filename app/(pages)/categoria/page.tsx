f0ccb8ee9477c9065af69719ac2b044b694b9071
jennicostag <83408210+jennicostag@users.noreply.github.com>
2025-12-10 11:02:27 -0300

    cor botao
    cor botao

--- app/(pages)/categoria/page.tsx
+++ app/(pages)/categoria/page.tsx
@@ -13,10 +13,10 @@ const Categoria = () => {
             <HeaderTitle title="PRODUTOS" />
             <div className="breadcrumb my-4">
                 <div className="container mx-auto px-4 uppercase text-sm">
-                    <span className={textColor ? "text-orange-500 mx-1" : "text-black mx-1"}><a href="/"> Nossos Produtos </a></span>
+                    <span className={textColor ? "text-orange-500 mx-1" : "text-black mx-1"}><a href="#"> Nossos Produtos </a></span>
                 </div>
             </div>
-            <NossosProdutos cor="" titulo="" corTitulo="" corBotao="" />
+            <NossosProdutos cor="" titulo="" corTitulo="text-white" corBotao="bg-blue-900" />
         </div>
     );
 };
